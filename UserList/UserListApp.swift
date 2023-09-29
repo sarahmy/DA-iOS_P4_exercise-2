8d97161527141aaddfc51b89f1f7c25f8bd3c9ae
bbloch <bbloch@veepee.com>
2023-09-29 11:37:27 +0200

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Bertrand Bloc'h on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
